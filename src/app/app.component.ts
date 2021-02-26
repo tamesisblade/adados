d5ec106f2b9ec02a5efbd7f17bdb7b202ef862d3
tamesis <hitlerblade123456@gmail.com>
2021-02-26 18:45:06 +0100

    introduccion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'adado';
+  dadoIzquierdo = '../assets/img/dice1.png';
+  dadoDerecho = '../assets/img/dice2.png';
+
+  numero1:number = 1;
+  numero2:number = 2;
+
+  tirarDados():void {
+    this.numero1 = Math.round( Math.random()*5 )+1;
+    this.numero2 = Math.round( Math.random()*5 )+1;
+   
+    this.dadoIzquierdo = '../assets/img/dice'+this.numero1+'.png';
+    this.dadoDerecho = '../assets/img/dice'+this.numero2+'.png';
+
+    
+  }
 }
